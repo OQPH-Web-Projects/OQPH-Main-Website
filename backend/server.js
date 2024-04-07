63740d6b6b10ede77530bd28266e1e9bbeccbd75
Xerxes Lance Laurenz A. Lompon <xerxeslancelaurenz.lompon@g.msuiit.edu.ph>
2024-04-07 22:44:44 +0800

    feat: connected server to mongodb atlas database

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const baseRoutes = require('./routes/base');
+const { mongoose } = require('mongoose');
 
 //express app
 const app = express();
@@ -14,12 +15,19 @@ app.use((req, res, next) => {
 });
 
 
-//routes
+// routes
 app.use(baseRoutes);
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT);
-});
+// connect to mongobb
+mongoose.connect(process.env.MONG_URI)
+    .then(() => {
+        //listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('connecting to db and listening on port', process.env.PORT);
+        });
+    })
+    .catch((error) => {console.log(error)})
+
+
 
 
