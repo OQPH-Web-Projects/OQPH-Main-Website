6b2c38dc3a1248ba047f6e0f5a42647ec68f946b
Xerxes Lance Laurenz A. Lompon <xerxeslancelaurenz.lompon@g.msuiit.edu.ph>
2024-04-08 10:09:06 +0800

    chore: moved the files to fronted folder

--- src/components/Button/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Button({ text }) {
-  return (
-    <button>{ text }</button>
-  )
-}
-
-export default Button;
--- src/components/Question/Question.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Question() {
-  return (
-    <div>Question</div>
-  );
-};
-
-export default Question;
